d5e3dbb5fd3de597a3529fc144f78199023c49af
KUNAL SAHU <53874235+mrkunalsahu@users.noreply.github.com>
2019-11-12 18:21:42 +0530

    Create sq.cpp

---
+++ sq.cpp
@@ -0,0 +1,17 @@
+
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+    clrscr();
+    int i,sqr;
+    while(i<11)
+    {
+        sqr = i*i;
+        cout<<"Square of "<<i<<" is "<<sqr<<endl;
+        i++;
+    }
+    getch();
+    
+            
+}
